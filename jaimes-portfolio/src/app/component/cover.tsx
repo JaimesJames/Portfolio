3a8aad35f50ba11249b98114c5f2b57bc07fcbd2
64070109Supanat <88423049+JaimesJames@users.noreply.github.com>
2023-11-30 03:40:57 +0700

    first com

---
+++ jaimes-portfolio/src/app/component/cover.tsx
@@ -0,0 +1,30 @@
+const Cover=()=>{
+    return(
+        <>
+        <div className="p-4 h-screen w-max flex flex-col content-center justify-center relative">
+            <h3 className="text-center text-xl">Hi, This is</h3>
+            <h1 className="text-center text-8xl"><span>Supanat</span>'s</h1>
+            <h3 className="text-center text-xl">Portfolio</h3>
+            <svg className="absolute " width="1520" height="1525" viewBox="0 0 1520 1525" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="1155.06" cy="571.06" r="22.1709" transform="rotate(45.5266 1155.06 571.06)" stroke="#2B2B2B"/>
+                <circle cx="1187.08" cy="1237.08" r="10.167" transform="rotate(45.5266 1187.08 1237.08)" fill="#2B2B2B" stroke="#2B2B2B"/>
+                <path d="M1209.23 1220.1C1123.88 1303.9 996.305 1328.61 858.965 1300.72C721.628 1272.83 574.586 1192.36 450.414 1065.88C326.243 939.409 248.484 790.913 223.126 653.086C197.767 515.257 224.816 388.156 310.167 304.359C395.518 220.563 523.095 195.855 660.435 223.743C797.772 251.63 944.814 332.105 1068.99 458.58C1193.16 585.055 1270.92 733.551 1296.27 871.377C1321.63 1009.21 1294.58 1136.31 1209.23 1220.1Z" stroke="#2B2B2B"/>
+            </svg>
+            <svg  className="absolute" width="1026" height="1026" viewBox="0 0 1026 1026" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="798.796" cy="630.767" r="64.9977" transform="rotate(45.5266 798.796 630.767)" fill="#2B2B2B" stroke="#2B2B2B"/>
+                <circle cx="512.631" cy="512.631" r="362" transform="rotate(45.5266 512.631 512.631)" stroke="#2B2B2B"/>
+            </svg>
+            <svg className="absolute -bottom-20 left-0"  width="307" height="307" viewBox="0 0 307 307" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="153.21" cy="153.21" r="107.84" transform="rotate(45.5266 153.21 153.21)" stroke="#2B2B2B"/>
+                <circle cx="261.904" cy="162.035" r="10.167" transform="rotate(45.5266 261.904 162.035)" fill="#2B2B2B" stroke="#2B2B2B"/>
+            </svg>
+
+
+
+
+
+        </div>
+        </>
+    )
+}
+export default Cover
---
+++ jaimes-portfolio/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+import Cover from './component/cover'
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Cover/>
+      </div>
+    </>
+  )
+}
