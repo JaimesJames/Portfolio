ff5f7dd048d32bc27c6c1f1f857dfdfe7839406c
64070109Supanat <88423049+JaimesJames@users.noreply.github.com>
2023-12-12 07:13:30 +0700

    update

--- jaimes-portfolio/src/app/layout.tsx
+++ jaimes-portfolio/src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'JaimesJames|Portfolio',
+  title: 'JaimesJames | Portfolio',
   description: 'I hope you like it!',
 }
 
