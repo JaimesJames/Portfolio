e82cff46eb712fc36d8378ba0fc5c0385fb85029
64070109Supanat <88423049+JaimesJames@users.noreply.github.com>
2024-01-05 13:08:06 +0700

    update

--- jaimes-portfolio/src/app/component/Artwork.tsx
+++ jaimes-portfolio/src/app/component/Artwork.tsx
@@ -31,11 +31,12 @@ const Artwork = () => {
             <div className="h-full w-full bg-gray-400 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-lg shadow-stone-800">
             <div className='w-full p-4'>
                 {/* <h2 className="text-white text-3xl font-bold text-center mb-10">3D Working</h2> */}
-                <div className='w-full flex md:p-10 xl:flex-row md:gap-10 flex-col gap-4 justify-center content-center m-auto'>
+                <div className='w-full flex md:p-10 xl:flex-row md:gap-10 flex-col gap-4 justify-center content-center m-auto flex-wrap'>
                     {/* <YouTubePlayer videoId="lbDm7tlKMAg" />
                     <YouTubePlayer videoId="md8lLVtfsL4" /> */}
-                    <iframe className='rounded-lg shadow-lg shadow-stone-800 w-full h-96'  src="https://www.youtube.com/embed/lbDm7tlKMAg?si=iVSAeQpgXWYbuLwm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-                    <iframe className='rounded-lg shadow-lg shadow-stone-800 w-full h-96'  src="https://www.youtube.com/embed/md8lLVtfsL4?si=PraiF3FfBngkqJUj" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+                    <iframe className='rounded-lg shadow-lg shadow-stone-800 xl:w-2/5 w-full md:h-96'  src="https://www.youtube.com/embed/lbDm7tlKMAg?si=iVSAeQpgXWYbuLwm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+                    <iframe className='rounded-lg shadow-lg shadow-stone-800 xl:w-2/5 w-full md:h-96'  src="https://www.youtube.com/embed/md8lLVtfsL4?si=PraiF3FfBngkqJUj" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+                    <iframe className='rounded-lg shadow-lg shadow-stone-800 xl:w-2/5 w-full md:h-96'  src="https://www.youtube.com/embed/PR19Yqi_ezc?si=zXeIrehgaHxMHYmd" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                 </div>
             </div>
 
